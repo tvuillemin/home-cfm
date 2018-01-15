6547a523a6d6290f35f02d2871a273d39212d419
Thibaut Vuillemin <contact@thibautvuillemin.com>
2018-01-15 13:58:54 +0100

    Minecraft: rolling snaphots

--- roles/minecraft/files/snapshot_minecraft.sh
+++ roles/minecraft/files/snapshot_minecraft.sh
@@ -2,4 +2,5 @@
 systemctl stop minecraft
 timestamp=$(date +%Y-%m-%d_%HH%M)
 zip -r /var/www/html/Minecraft/$timestamp /opt/minecraft/world
+find /var/www/html/Minecraft/ -mtime +7 -delete
 systemctl start minecraft
